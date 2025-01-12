4e82ae649084bb20edfc7d6abeb48655c3094e78
alberba <albertsalomv@gmail.com>
2025-01-12 19:18:58 +0100

    porfavor

--- 18_Practica2/test.py
+++
@@ -1,8 +0,0 @@
-import scipy.io
-
-# Cargar el archivo .mat
-mat_file = './18_Practica2/annotation_0001.mat'
-data = scipy.io.loadmat(mat_file)
-
-# Mostrar las claves del diccionario
-print(data.keys())
